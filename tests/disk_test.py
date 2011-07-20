57c37ccfc99b0a7de80ac96049aab7c1a22d15e4
Eric Plaster <plaster@gmail.com>
2011-07-20 12:18:29 -0500

    Added global method to setup the vix config file Tear down now deletes the created vmdk Now tests transport modes

--- tests/disk_test.py
+++ tests/disk_test.py
@@ -5,56 +5,63 @@ Created on Jul 8, 2011
 '''
 import unittest, os, os.path
 from vixDiskLib import VixDisk, VixDiskLib_CreateParams, VixDiskLib_DefaultBlockSize
-from vixDiskLib.consts import VixDiskLibDiskType, VixDiskLibAdapterType, VixDiskLibHwVersion
+from vixDiskLib.consts import VixDiskLibDiskType, VixDiskLibAdapterType, VixDiskLibHwVersion, VixDiskTransportModes
 import numpy as np
 
+def setupConfigs():
+    test_dir = os.path.abspath(os.path.dirname(__file__))
+    vix_config = os.path.join( test_dir, 'vix_disklib.cfg')
+    
+    if not os.path.exists(vix_config):
+        data = open( vix_config+'.in' ).read()
+        data = data.replace('TRANSPORT_LOGLEVEL', '6')
+        data = data.replace('NFC_LOGLEVEL', '4')
+        data = data.replace('TEMP_DIR', test_dir)
+        open( vix_config, 'w').write(data)
+    return vix_config
+        
 class TestLocalDisk(unittest.TestCase):
 
     def setUp(self):
-        # setup the config file...
-        self.test_dir = os.path.abspath(os.path.dirname(__file__))
-        config = os.path.join( self.test_dir, 'test.cfg')
-        data = open( config+'.in' ).read()
-        data = data.replace('TRANSPORT_LOGLEVEL', '6')
-        data = data.replace('NFC_LOGLEVEL', '4')
-        data = data.replace('TEMP_DIR', self.test_dir)
-        open( config, 'w').write(data)
+        vix_config = setupConfig() # make sure that the config file exists
         
+        self.test_dir = os.path.abspath(os.path.dirname(__file__))
+        self.test_disk = os.path.join(self.test_dir, "test.vmdk")
+       
         self.block_size = 1024
+        self.test_disk_blocks = 1048576 # 1GB
         
         # open a local disk
-        self.disk = VixDisk(libdir="/usr/lib/vmware-vix-disklib/lib64", config=config, block_size=self.block_size)
+        self.disk = VixDisk(libdir="/usr/lib/vmware-vix-disklib/lib64", vix_config=config, block_size=self.block_size)
         self.disk.connect(readonly=False)
 
     def tearDown(self):
         self.disk.disconnect()
+        if os.path.exists(self.test_disk):
+            os.unlink(self.test_disk)
 
-    def testModes(self):
+    def testAvailableModes(self):
         modes = self.disk.available_modes
         
         # as a minimum, we should have these modes available
         for mode in ['file', 'ndbssl', 'ndb']:
             self.assertIn('file', modes, 'Transport mode %s is not available' % mode)
         
+    def testGettingMode(self):
+        mode = self.disk.transport_mode
+        self.assertIn(mode, VixDiskTransportModes, "Got an invalid transport mode: %s" % mode)
+        
+    def testBlockSize(self):
+        self.assertEqual(self.disk.block_size, self.block_size, 
+                         "Block sizes didn't match: %d %d" % (self.block_size, self.disk.block_size))
+        
     def testCreate(self):
-        blocks = 1048576 # should be about a 1G disk
-        params = VixDiskLib_CreateParams(
-                disk_type = VixDiskLibDiskType['MONOLITHIC_SPARSE'],
+        params = VixDiskLib_CreateParams( disk_type = VixDiskLibDiskType['MONOLITHIC_SPARSE'],
                 adapter_type = VixDiskLibAdapterType['SCSI_LSILOGIC'],
-                hw_version = VixDiskLibHwVersion['CURRENT'],
-                blocks = blocks)
+                hw_version = VixDiskLibHwVersion['CURRENT'], blocks = self.test_disk_blocks)
         
-        filename = os.path.join(self.test_dir, "test.vmdk")
-        
-        # if we already ran the test...  delete the test files
-        if os.path.exists(filename):
-            os.unlink(filename)
-        
-        if os.path.exists(filename+".lck"):
-            os.unlink(filename+".lck")
-        
-        self.disk.create(filename, params)
-        self.disk.open(filename)
+        self.disk.create(self.test_disk, params)
+        self.disk.open(self.test_disk)
         
         # fill up a 1k block of data
         buffer = np.zeros(self.block_size, dtype=np.uint8)
@@ -67,7 +74,7 @@ class TestLocalDisk(unittest.TestCase):
         
         # file sizes should be the same, but we'll give a little wiggle room in case header 
         # info is different with different filesystems
-        self.assertAlmostEqual(os.stat(filename), 1073938432, 
+        self.assertAlmostEqual(os.stat(self.test_disk), 1073938432, 
                                "File size doesn't match what we where aiming for...", delta=1024)
         
         
