72f60f9cb6e636cc9155b383facd8e4cda4d08df
Eric Plaster <plaster@gmail.com>
2011-07-21 13:14:27 -0500

    Added distutils directives to compile up the modules making the setup.py file much much cleaner

--- setup.py
+++ setup.py
@@ -26,62 +26,7 @@ vixDiskLib - python wrapper for vixDiskLib (in C)
 '''
 
 from distutils.core import setup
-from Cython.Distutils import extension, build_ext
-import subprocess, os, time, sys
-import os.path
-
-# make sure when we import vixDiskLib that it's the one in the current directory
-sys.path.insert(0, os.path.abspath(__file__))
-
-#################################################################################
-# Helper functions for setup
-#################################################################################
-def run(cmd, wait=True, timeout=30):
-    result = ""
-
-    p = subprocess.Popen(cmd, shell=False, env=os.environ, stdin=subprocess.PIPE,
-            stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    if wait:
-        result = p.communicate("\n")[0].strip()
-        if p.returncode != 0:
-            raise RuntimeError('Error executing %r: %r' % (cmd, result))
-    else:
-        p.stdin.write("\n")
-        for x in range(timeout):
-            rc = p.poll()
-            if rc == None:
-                # process hasn't terminated yet...
-                print "%d seconds and still running..." % x
-                time.sleep(1)
-            else:
-                break
-    return result.strip()
-
-def run_pkg_config(packages):
-    args = ['pkg-config', '--libs', '--cflags'] + list(packages)
-    return run(args)
-
-def pkgconfig(*packages, **kwargs):
-    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    for token in run_pkg_config(packages).split():
-        kwargs.setdefault(flag_map.get(token[:2]), []).append(token[2:])
-    return kwargs
-
-#################################################################################
-# Setup the libraries and include paths for vixDiskLib (VDDK)
-#################################################################################
-kw = pkgconfig('vix-disklib')
-# we need to make sure that we include the vixDiskLibVim library
-kw['libraries'].append('vixDiskLibVim')
-kw['include_dirs'].append('vixDiskLib')
-print kw
-ext_modules = [
-    extension.Extension('vixDiskLib.vixBase', ['vixDiskLib/vixBase.pyx'], **kw), #IGNORE:W0142
-    extension.Extension('vixDiskLib.vixDiskBase', ['vixDiskLib/vixDiskBase.pyx'], **kw), #IGNORE:W0142
-]
-
-# we need to make sure we have these to python modules in our path
-install_requires = ["cython"]
+from Cython.Build import cythonize
 
 setup( 
     name = 'vixDiskLib',
@@ -92,11 +37,8 @@ setup(
     long_description = "vSphere SDK for Python",
     url = "https://github.com/xuru/vixDiskLib",
     platforms=["any"],
-    zip_safe=False,
     license = "MIT",
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = ext_modules,
-    install_requires = install_requires,
+    ext_modules = cythonize(['vixDiskLib/vixBase.pyx', 'vixDiskLib/vixDiskBase.pyx']),
     packages = ["vixDiskLib"],
     classifiers = ['Development Status :: 4 - Beta',
                    'Framework :: VMWare',
--- vixDiskLib/vixBase.pxd
+++ vixDiskLib/vixBase.pxd
@@ -1,3 +1,6 @@
+# distutils: language = C
+# distutils: libraries = vixDiskLib vixMntapi
+# distutils: include_dirs = /usr/lib/vmware-vix-disklib/include
 
 from common cimport *
 from vddk cimport *
